e116f37a2650e15c822a3f610a412c746de33221
Danila-Milovanov <9019853@student.zadkine.nl>
2025-02-12 09:31:30 +0100

    HerstelOpdracht_v1.1
    Comments in het code

--- Herstelopdracht/verwerk_registratie.php
+++ Herstelopdracht/verwerk_registratie.php
@@ -4,10 +4,11 @@ session_start();
 function valideerInvoer($data) {
     return htmlspecialchars(trim($data));
 }
-
+    // controle dat formulier is toegevoegt
 if ($_SERVER['REQUEST_METHOD'] == 'POST') {
     $fouten = [];
 
+    // values
     $naam = !empty($_POST['naam']) ? valideerInvoer($_POST['naam']) : $fouten[] = "Naam is verplicht.";
     if (isset($naam) && (strlen($naam) < 3 || strlen($naam) > 15)) {
         $fouten[] = "De naam moet tussen 3 en 15 tekens lang zijn.";
@@ -34,15 +35,17 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST') {
 
     $stad = !empty($_POST['stad']) && in_array($_POST['stad'], ['Amsterdam', 'Rotterdam', 'Utrecht', 'Den Haag']) ? valideerInvoer($_POST['stad']) : $fouten[] = "Kies een geldige stad.";
 
-    
+    // if errors - feedback
     if ($fouten) {
         echo "<h2>Fouten:</h2><ul><li>" . implode("</li><li>", $fouten) . "</li></ul><a href='javascript:history.back()'>Ga terug</a>";
         exit;
     }
 
+    // no errors - opslaan
     $_SESSION['naam'] = $naam;
     $_SESSION['tijdslot'] = $tijdslot;
 
+    // redirect
     header("Location: bevestiging.php");
     exit;
 } else {
